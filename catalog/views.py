8f788ae8b7ed185537e00cd6ba4e15f17053f12a
lotan <lotanable2@gmail.com>
2022-06-18 10:26:22 +0300

    lotan

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,7 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_genre=Genre.objects.count()
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -18,6 +19,7 @@ def index(request):
 
     context = {
         'num_books': num_books,
+        'num_genre':num_genre,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
